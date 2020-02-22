f0e802ec49b089d4930e6978379de9de75ccae40
Kvye thinkpad <Karlallanjohnvye@gmail.com>
2020-02-22 20:24:07 +0000

    Created functionality for writing to json with a web form
    Created functionality for the app to allow for user input to enter a message into a form and submit it, writes the data to a json file and displays it onto the index.html page.
    
    Updated requirements.txt to include json and datetime

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import datetime
+import json
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    """returns the index html page"""
+    if request.method == "POST": # if we get a POST request
+        chatfield = request.form["chatfield"] # take what we got from the chatfield form's request
+        posttime = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") # get current datetime
+        
+        postdict = { # put the chatfield form info and posttime into a dict
+            "date/time" : str(posttime),
+            "message" : chatfield
+        }
+        postdata = json.dumps(postdict) # write it into a json file
+        with open("data/messages.json", "w") as f:
+            f.write(postdata)
 
+        with open("data/messages.json", "r") as f: # read the json file
+            chat = json.load(f)
+
+        return render_template("index.html", chat=chat) # return index.html along with the json file info
+    
+    return render_template("index.html") # otherwise just GET request for index.html
+
+
+if __name__ == "__main__":
+    app.run()
