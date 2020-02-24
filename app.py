65613d71b426f84dd81dea0ffa840430b3337a1f
Kvye thinkpad <Karlallanjohnvye@gmail.com>
2020-02-24 19:05:45 +0000

    Updated app to store and display multiple messages
    Updated the app to store and display multiple messages, appends each new message to a json file that is read and displayed in the HTML file.

--- app.py
+++ app.py
@@ -10,17 +10,19 @@ def index():
         chatfield = request.form["chatfield"] # take what we got from the chatfield form's request
         posttime = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") # get current datetime
         
-        postdict = { # put the chatfield form info and posttime into a dict
-            "date/time" : str(posttime),
-            "message" : chatfield
-        }
-        postdata = json.dumps(postdict) # write it into a json file
-        with open("data/messages.json", "w") as f:
-            f.write(postdata)
-
-        with open("data/messages.json", "r") as f: # read the json file
-            chat = json.load(f)
-
+        with open("data/messages.json") as fr:# open messages.json
+            discussiondata = json.load(fr)# and load the json from it
+            temp = discussiondata["discussion"]# take just the list
+            entry = {"date/time" : str(posttime), "message" : chatfield}# create an entry
+            temp.append(entry)# append the entry to the list
+        
+        with open("data/messages.json", "w") as fw:
+            json.dump(discussiondata, fw, indent=4)# now write the file with the newly appended json
+        
+        with open("data/messages.json") as fr:
+            chatdata = json.load(fr)# assign the newly appended json to something we can return
+            chat = chatdata["discussion"]
+        
         return render_template("index.html", chat=chat) # return index.html along with the json file info
     
     return render_template("index.html") # otherwise just GET request for index.html
